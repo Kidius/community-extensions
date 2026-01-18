ccb0714a451d738fd93e74d42b6c693f8aba3ca1
Kidius <68790653+Kidius@users.noreply.github.com>
2026-01-18 04:13:31 +0000

    Deploying to gh-pages from @ Kidius/community-extensions@a5252c5a044b6361f1ceac0db9a57a38822557a5 🚀

--- 0.8/MangaDex/source.js
+++ 0.8/MangaDex/source.js
@@ -3804,7 +3804,7 @@ var _Sources = (() => {
       return App.createDUIOAuthButton({
         id: "mdex_oauth",
         label: "Login with MangaDex",
-        authorizeEndpoint: "https://auth.mangadex.dev/realms/mangadex/protocol/openid-connect/auth",
+        authorizeEndpoint: "https://auth.mangadex.org/realms/mangadex/protocol/openid-connect/auth",
         clientId: "thirdparty-oauth-client",
         redirectUri: "paperback://mangadex-login",
         responseType: {
@@ -3812,7 +3812,7 @@ var _Sources = (() => {
           pkceCodeLength: 64,
           pkceCodeMethod: "S256",
           formEncodeGrant: true,
-          tokenEndpoint: "https://auth.mangadex.dev/realms/mangadex/protocol/openid-connect/token"
+          tokenEndpoint: "https://auth.mangadex.org/realms/mangadex/protocol/openid-connect/token"
         },
         async successHandler(accessToken2, refreshToken) {
           await saveAccessToken(stateManager, accessToken2, refreshToken);
